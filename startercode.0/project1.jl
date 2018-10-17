a5264db1c0d9bb49d0fb3fe03fb4728fe29ceb26
Somrita Banerjee <somrita@stanford.edu>
2018-10-17 16:21:15 -0700

    M values computed correctly for my small example graph

--- startercode.0/project1.jl
+++ startercode.0/project1.jl
@@ -2,6 +2,7 @@ using LightGraphs
 using Printf
 using DataFrames
 using CSV
+using GraphPlot
 
 """
     write_gph(dag::DiGraph, idx2names, filename)
@@ -16,52 +17,37 @@ function write_gph(dag::DiGraph, idx2names, filename)
     end
 end
 
-function calculateParentalInstantiationIndex(parents_i, parentalValues, r)
-    r_parents_i=r[parents_i]; # Number of instantiations of each parent
-    A=fill(1,Tuple(r_parents_i)); # Array with dimensions corresponding to each parent
-    linearIndices=LinearIndices(A);
-    return linearIndices[parentalValues...] # Using splat operator to index into linearIndices
-end
-
-# EXTRA DELETE
-# say the parents have [rA rB] possible values
-# Ordering [1 1] [1 2] [1 3]... [1 rB] [2 1] [2 2] [2 3]... [2 rB]
-# Therefore [i j]=rB*(i-1)+j. Similarly [i j k]=rB*rC*(i-1)+rC*(j-1)+k
 # Bayes nets.jl
 function compute(infile, outfile)
+    println("Computing infile to outfile")
     # Read the data available
-    df=CSV.File(infile) |> DataFrame;
-    n= length(df); # i from 1 to n number of variables
+    data=CSV.File(infile) |> DataFrame;
+    n= length(data); # i from 1 to n number of variables
     # Guess a graph
     G0= DiGraph(n);
     # compute score of current graph
-    BayesianScore=calculateBayesianScore(G0)
+    BayesianScore=calculateBayesianScore(data,G0)
     someConditionOnScore=false;
     numberOfAttempts=0;
     while (someConditionOnScore=false && numberOfAttempts<=100)
-        # make changes to the graph
-        # recompute score of new graph
+        # TODO make changes to the graph
+        # TODO recompute score of new graph
         # increment numberOfAttempts
-        numberOfAttempts ++;
+        numberOfAttempts+=1;
     end
-    # write the latest graph to the outfianSile
+    # write the latest graph to the outfile
     write_gph(dag::DiGraph, idx2names, filename)
 end
 
-function calculateBayes
+function calculateBayesianScore(df::DataFrame, G::DiGraph)
+    println("Calculating Bayesian score for a particular graph")
     n= length(df); # i from 1 to n number of variables
     r= describe(df).max; # index i has number of possible values ri for each variable
-    parents=Array{Any}(undef, n); # parents[i] has the indices of parents of i
-    q=ones(Int64, n); # q[i] has number of parental instantiations qi
+    parents=findParents(df,G); # parents[i] has the indices of parents of i
+    q=zeros(Int64, n); # q[i] has number of parental instantiations qi
     M=Array{Any}(undef, n);
     for i = 1: n
-        M[i]=zeros(Int64, q[i], r[i]);
-        # access an element by looking at M[i][j,k]
-        # if i=1
-        #     parents[i]=[3,4];
-        # else
-            parents[i]=Int64[];
-        # end
+        # Find q (number of parental instantiations) for each variable i
         if (length(parents[i])==0)
             q[i]=1;
         else
@@ -69,30 +55,53 @@ function calculateBayes
                 q[i]+=r[p];
             end
         end
+        # instantiate M with guess of prior which is uniform
+        M[i]=zeros(Int64, q[i], r[i]);
+        # access an element by looking at M[i][j,k]
     end
     for i=1: n
-        parents_i=parents[i];
-        # aggregate by itself and its parents
-        aggs= by(df,vcat(i,parents_i),nrow);
+        parentIndices=parents[i];
+        # aggregate data by itself and its parents
+        aggs= by(df,vcat(i,parentIndices),nrow);
         for row in eachrow(aggs)
             # i= current variable=i
             # j=parental instantiation index for combination(row[2:end-1])
-            parentalValues=row[2:end-1];
-            if (length(parentalValues) ==0)
+            parentValues=row[2:end-1];
+            if (length(parentValues) ==0)
                 j=1;
             else
-                j=calculateParentalInstantiationIndex(parents_i, parentalValues, r);
+                parentValues=convert(Array, parentValues)
+                j=calculateParentalInstantiationIndex(parentIndices, parentValues, r);
             end
-            k=row[1]; # Actual value of the variable i
-            incrementM=row[end]; # Mijk increment = row[end]
+            k=row[1]; # Actual value of the variable i (first column of aggregation)
+            incrementM=row[end]; # Mijk increment = row[end] (last column of aggregation)
             M[i][j,k]+=incrementM;
         end
     end
-    CSV.write(outfile, df)
-    # WRITE YOUR CODE HERE
-     # FEEL FREE TO CHANGE ANYTHING ANYWHERE IN THE CODE
-    # THIS INCLUDES CHANGING THE FUNCTION NAMES, MAKING THE CODE MODULAR, BASICALLY ANYTHING
+    return M
+    # TODO Find score as a result of the M values
+    # TODO return score
+end
 
+function findParents(df::DataFrame, G::DiGraph)
+    n= length(df);
+    # parents=Array{Array{Int64},1}(undef,n);
+    # fill!(parents,[]); DONT DO THIS. IT FILLS WITH REFERENCE INSTEAD OF VALUE.
+    parents=[Int64[] for i=1:n]
+    edgesOfG=collect(edges(G));
+    # TODO Look through edges of graph and find all the parents
+    for e in edgesOfG
+        par, chi= src(e), dst(e);
+        push!(parents[chi],par);
+    end
+    return parents;
+end
+
+function calculateParentalInstantiationIndex(parentIndices, parentValues, r)
+    r_parents=r[parentIndices]; # Number of instantiations of each parent
+    A=fill(1,Tuple(r_parents)); # Array with dimensions corresponding to each parent
+    linearIndices=LinearIndices(A);
+    return linearIndices[parentValues...] # Using splat operator to index into linearIndices
 end
 
 # if length(ARGS) != 2
@@ -102,7 +111,15 @@ end
 # inputfilename = ARGS[1]
 # outputfilename = ARGS[2]
 
-inputfilename = "small.csv"
-outputfilename= "smallout.csv"
-
-compute(inputfilename, outputfilename)
+# inputfilename = "small.csv"
+# outputfilename= "smallout.csv"
+#
+# compute(inputfilename, outputfilename)
+data=CSV.File("myownsmallexample.csv") |> DataFrame;
+n= length(data); # i from 1 to n number of variables
+# Guess a undirected graph
+G= DiGraph(n);
+gplot(G, nodelabel=1:n)
+add_edge!(G, 1, 2);
+# compute score of current graph
+M=calculateBayesianScore(data,G)
